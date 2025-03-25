b45429881920bd62716790b984a2aed395afce98
Pavan Pawar <pavanrpawar36@gmail.com>
2025-03-25 17:51:02 +0530

    Appwrite database, file upload and custom queries

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -5,6 +5,7 @@ function useCurrencyInfo(currency) {
 
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        // fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
         // .then((res) => setData(res.currency))        // same like line no. 9
