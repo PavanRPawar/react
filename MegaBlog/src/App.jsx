a81f80294c0a6f5b7594f6e3e16216f7e2f3901a
Pavan Pawar <pavanrpawar36@gmail.com>
2025-03-25 14:47:38 +0530

    ENV and Appwrite

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // console.log(process.env.REACT_APP_APPWRITE_URL);       // For Create React App
+  console.log(import.meta.env.VITE_APPWRITE_URL); // For Vite App
+  
   return (
     <>
       <h1>Hello React</h1>
